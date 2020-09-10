445d88773c8999109b5dbe46c03be05b842cc9f1
Your Name <you@example.com>
2020-09-10 08:10:43 +0300

    adding some stuff to input.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+require('dotenv').config('.env');
 
-mongoose.connect('mongodb://localhost:27017/notesy', {
+const URL = process.env.URL;
+
+mongoose.connect(URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 }) .then(() => {
   console.log('connected');
 }).catch((err) => console.log(err));
@@ -21,27 +26,27 @@ notes.add(myInputs);
 
 
 
-if (myInputs.valid()) {
-  notes.execute(myInputs.command)
-    .then(result => {
-      if (myInputs.command == 'List') {
-        const notes = result;
-        notes.forEach(note => {
-          console.log(note.text);
-          console.log('');
-          console.log(` Category: ${note.category}\t`);
-          console.log('--------------------------------\n');
-        });
-      }
-    })
-    .then(mongoose.disconnect)
-    .catch(error => console.error(error));
-} 
-else {
-  help();
-}
+// if (myInputs.valid()) {
+//   notes.execute(myInputs.command)
+//     .then(result => {
+//       if (myInputs.command == 'List') {
+//         const notes = result;
+//         notes.forEach(note => {
+//           console.log(note.text);
+//           console.log('');
+//           console.log(` Category: ${note.category}\t`);
+//           console.log('--------------------------------\n');
+//         });
+//       }
+//     })
+//     .then(mongoose.disconnect)
+//     .catch(error => console.error(error));
+// } 
+// else {
+//   help();
+// }
 
-function help () {
-  console.log('Error!');
-  process.exit();
-}
+// function help () {
+//   console.log('Error!');
+//   process.exit();
+// }
